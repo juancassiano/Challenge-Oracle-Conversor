aa47e27e55d71b790a460b05e33077a85edffa9c
Juan Cassiano <juancassiano@hotmail.com>
2023-02-23 13:40:36 -0300

    :tada: init project

---
+++ src/Conversor.java
@@ -0,0 +1,88 @@
+
+import javax.swing.JOptionPane;
+
+public class Conversor {
+	
+	public static void main(String[] args) {
+		
+		Double valor = null;
+		Double resultado = null;
+
+		String[] opcoes = {"Conversor de Moeda", "Conversor de Temperatura"};
+		String[] tipoConversao = {"Dolar para Real", "Euro para Real", "Libra Esterlina para Real",
+				"Peso Argentino para Real", "Peso Chileno para Real", "Real para Dolar", "Real para Euro",
+				"Real para Libra Esterlina", "Real para Peso Argentino", "Real para Peso Chileno" };
+		
+		String type = JOptionPane.showInputDialog(null, "Escolha uma opção:","Menu",
+				JOptionPane.PLAIN_MESSAGE, null,opcoes,null)
+						.toString();
+					
+	try {
+			String input = JOptionPane.showInputDialog("Insira um Valor:");
+			valor = Double.parseDouble(input);
+		
+		
+		String escolha = JOptionPane.showInputDialog(null, "Escolha a moeda para a qual você deseja girar seu dinheiro","Moeda",
+				JOptionPane.PLAIN_MESSAGE, null, tipoConversao,null)
+				.toString();
+		
+		Moeda converterMoeda = new Moeda();
+			switch(escolha) {
+				case "Dolar para Real":
+					resultado = converterMoeda.converterDolarEmReal(valor);
+					break;
+					
+				case "Euro para Real":
+					resultado = converterMoeda.converterEuroEmReal(valor);
+					break;
+					
+				case "Libra Esterlina para Real":
+					resultado = converterMoeda.converterLibraEsterlinaEmReal(valor);
+					break;
+					
+				case "Peso Argentino para Real":
+					resultado = converterMoeda.converterPesoArgentinoEmReal(valor);
+					break;
+					
+				case "Peso Chileno para Real":
+					resultado = converterMoeda.converterPesoChilenoEmReal(valor);
+					break;
+					
+				case "Real para Dolar":
+					resultado = converterMoeda.converterRealEmDolar(valor);
+					break;
+					
+				case "Real para Euro":
+					resultado = converterMoeda.converterRealEmEuro(valor);
+					break;
+					
+				case "Real para Libra Esterlina":
+					resultado = converterMoeda.converterRealEmLibraEsterlina(valor);
+					break;
+					
+				case "Real para Peso Argentino":
+					resultado = converterMoeda.converterRealEmPesoArgentino(valor);
+					break;
+					
+				case "Real para Peso Chileno":
+					resultado = converterMoeda.converterRealEmPesoChileno(valor);
+					break;
+					
+				default:
+					break;
+			}
+			
+			JOptionPane.showMessageDialog(null, resultado);
+			
+		} catch (NumberFormatException e) {
+			JOptionPane.showMessageDialog(null, "O valor digitado não é um número");
+		} finally {
+			int resposta = JOptionPane.showConfirmDialog(null, "Deseja realizar outra conversão?");
+			if(resposta == JOptionPane.YES_OPTION) {
+				main(args);
+			}
+		}
+		
+	}
+
+}
---
+++ src/Moeda.java
@@ -0,0 +1,49 @@
+
+public class Moeda {
+	public static final double REAL_DOLAR = 0.19;
+	public static final double REAL_EURO = 0.18;
+	public static final double REAL_LIBRA_ESTERLINA = 0.16;
+	public static final double REAL_PESO_ARGENTINO = 38.22;
+	public static final double REAL_PESO_CHILENO = 156.7;
+	
+	public static final double DOLAR_REAL = 5.14;
+	public static final double EURO_REAL = 5.51;
+	public static final double LIBRA_ESTERLINA_REAL = 6.21;
+	public static final double PESO_ARGENTINO_REAL = 0.03;
+	public static final double PESO_CHILENO_REAL = 0.03;
+	
+	
+	public double converterRealEmDolar(double valorInserido) {
+		return valorInserido * REAL_DOLAR;
+	}
+	public double converterRealEmEuro(double valorInserido) {
+		return valorInserido * REAL_EURO;
+	}
+	public double converterRealEmLibraEsterlina(double valorInserido) {
+		return valorInserido * REAL_LIBRA_ESTERLINA;
+	}
+	public double converterRealEmPesoArgentino(double valorInserido) {
+		return valorInserido * REAL_PESO_ARGENTINO;
+	}
+	public double converterRealEmPesoChileno(double valorInserido) {
+		return valorInserido * REAL_PESO_CHILENO;
+	}
+	
+	public double converterDolarEmReal(double valorInserido) {
+		return valorInserido * DOLAR_REAL;
+	}
+	public double converterEuroEmReal(double valorInserido) {
+		return valorInserido * EURO_REAL;
+	}
+	public double converterLibraEsterlinaEmReal(double valorInserido) {
+		return valorInserido * LIBRA_ESTERLINA_REAL;
+	}
+	public double converterPesoArgentinoEmReal(double valorInserido) {
+		return valorInserido * PESO_ARGENTINO_REAL;
+	}
+	public double converterPesoChilenoEmReal(double valorInserido) {
+		return valorInserido * PESO_CHILENO_REAL;
+	}
+	
+}
+	
